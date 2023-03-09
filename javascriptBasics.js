3729ab87ebec0d242e4cf9d24544992bad214595
Fernanda Cantú <fercantu56@gmail.com>
2023-03-09 11:41:53 -0600

    javabasics created, functions declared

---
+++ javascriptBasics.js
@@ -0,0 +1,95 @@
+// console.log for outputs
+// 1.- 
+function noRepeat()
+{
+
+}
+
+// 2.-
+function bubbleSort()
+{
+
+}
+
+// 3.- 
+function invertArray()
+{
+
+}
+
+function modifyArray()
+{
+
+}
+
+// 4.-
+function interCaps()
+{
+
+}
+
+// 5.- 
+function maxCommonDivider()
+{
+
+}
+
+// 6.-
+function hackerSpeak()
+{
+
+}
+
+// 7.-
+function listMaker()
+{
+
+}
+
+// 8.- 
+function removeDuplicates()
+{
+
+}
+
+// 9.- 
+function shortestList()
+{
+
+}
+
+// 10.-
+function palindrome()
+{
+
+}
+
+// 11.-
+function alphOrder()
+{
+
+}
+
+// 12.- 
+function mediumMode()
+{
+
+}
+
+// 13.-
+function mostFrequentString()
+{
+
+}
+
+// 14.- 
+function powerTwo ()
+{
+
+}
+
+// 15.-
+function descendingOrder()
+{
+    
+}
