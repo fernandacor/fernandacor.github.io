958439cedf85331c05cc5a2f3ed8ddc978515e07
Fernanda Cantú <fercantu56@gmail.com>
2023-03-09 16:58:49 -0600

    small changes in jsbasics

--- javascriptBasics.js
+++ javascriptBasics.js
@@ -5,11 +5,37 @@ function noRepeat()
 
 }
 
-// 2.-
-function bubbleSort()
+// 2.- Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lista de números.
+function bubbleSorter()
 {
 
+    {
+        for (let i; i = bubbleSort.length; i++)
+        {
+            
+        }
+
+    }
+
 }
+
+const bubbleSort = arr => {
+    const l = arr.length;
+    for (let i = 0; i < l; i++ ) {
+      for (let j = 0; j < l - 1 - i; j++ ) {
+        if ( arr[ j ] > arr[ j + 1 ] ) {
+          [ arr[ j ], arr[ j + 1 ] ] = [ arr[ j + 1 ], arr[ j ] ];
+        }
+      }
+    }
+  
+    return arr;
+  };
+  
+  const arr = [10, 4, 40, 32, 67, 12, 43, 31, 65, 1];
+  const result = bubbleSort(arr);
+  
+  result;
 
 // 3.- 
 function invertArray()
@@ -22,7 +48,7 @@ function modifyArray()
 
 }
 
-// 4.-
+// 4.- Escribe una función que reciba una cadena de texto y regrese una nueva con la primer letra de cada palabra en mayúscula.
 function interCaps()
 {
 
@@ -34,10 +60,12 @@ function maxCommonDivider()
 
 }
 
-// 6.-
+// 6.-Crea una función que cambie una cadena de texto a 'Hacker Speak'. Por ejemplo, para la cadena 'Javascript es divertido', su hacker speak es: 'J4v45c1pt 35 d1v3rt1d0'.
 function hackerSpeak()
 {
-
+    // if A existe, replace it por 4, E = 3, I = 1, o = 0, U = ??
+    // recorrer el array
+    // console.log(cadena.replace('A', '4'))
 }
 
 // 7.-
@@ -46,7 +74,7 @@ function listMaker()
 
 }
 
-// 8.- 
+// 8.- Escribe una función que quite los elementos duplicados de un arreglo y regrese una lista con los elementos que quedan. Por ejemplo: quitaDuplicados([1, 0, 1, 1, 0, 0]) -> [1, 0]
 function removeDuplicates()
 {
 
@@ -58,7 +86,7 @@ function shortestList()
 
 }
 
-// 10.-
+// 10.-Escribe una función que revise si una cadena de texto es un palíndromo o no.
 function palindrome()
 {
 
@@ -70,7 +98,7 @@ function alphOrder()
 
 }
 
-// 12.- 
+// 12.- Escribe una función que tome una lista de números y devuelva la mediana y la moda.
 function mediumMode()
 {
 
@@ -82,14 +110,14 @@ function mostFrequentString()
 
 }
 
-// 14.- 
-function powerTwo ()
-{
-
-}
-
-// 15.-
-function descendingOrder()
+// 14.- Escribe una función que tome un número y devuelva verdadero si es una potencia de dos, falso de lo contrario.
+function powerTwo (num)
 {
     
+}
+
+// 15.- Escribe una función que tome una lista de números y devuelva una nueva lista con todos los números en orden descendente.
+function descendingOrder()
+{
+    //esto se puede hacer con un for loop y pones como una variable vacia para que se recorran los valores
 }
