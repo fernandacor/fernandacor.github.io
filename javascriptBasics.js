a46740b078a5103850bc847b0c124ed8a3ef8dae
Fernanda Cantú <fercantu56@gmail.com>
2023-03-14 21:16:52 -0600

    faltan la 6 y la 13

--- javascriptBasics.js
+++ javascriptBasics.js
@@ -1,8 +1,11 @@
-// console.log for outputs
+//ACTIVIDAD EN CLASE: JAVASCRIPT
+
 // EJERCICIO 1
-function primerNumeroQueNoSeRepite(str){
+function primerNumeroQueNoSeRepite(str)
+{
   const freq = {};
-  for(let i = 0; i < str.length; i++){
+  for(let i = 0; i < str.length; i++)
+  {
       const char = str[i];
       // Al utlizar un tipo de "diccionario" utilizamos el operador condicional terciario 
       // para determinar si el caracter está en este, si es así este le suma 1 a ese caracter,
@@ -10,10 +13,12 @@ function primerNumeroQueNoSeRepite(str){
       freq[char] = freq[char] ? freq[char] + 1 : 1;
   }
 
-  for(let i = 0; i < str.length; i++) {
+  for(let i = 0; i < str.length; i++) 
+  {
       const char = str[i];
       // Busca el caracter que tenga asignado el valor 1, si lo encuentra va a hacer el return
-      if(freq[str[i]] === 1){
+      if(freq[str[i]] === 1)
+      {
           return char;
       }
   }
@@ -22,12 +27,12 @@ function primerNumeroQueNoSeRepite(str){
 }
 
 //console.log('Ejercicio 1: '+ primerNumeroQueNoSeRepite('abacddbec'));
-console.log('Ejercicio 1: Función que encuentre el primer carácter de una cadena de texto que no se repite.');
+console.log('EJERCICIO 1: Función que encuentre el primer carácter de una cadena de texto que no se repite.');
 console.log('String: (abacddbec)');
 console.log(primerNumeroQueNoSeRepite('abacddbec'));
 
 
-// 2.- Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lista de números.
+// EJERCICIO 2
 function bubbleSorter(numList)
 {
   for (let i = 0; i < numList.length; i++)
@@ -42,51 +47,40 @@ function bubbleSorter(numList)
   }
 }
 
-console.log('Ejercicio 2: Bubblesort');
+console.log('EJERCICIO 2: Función que implementa el algoritmo bubblesort para ordenar una lista de números.');
 console.log('Original list: [3, 7, 9, 10, 0, 2, 5, 1, 8, 4]');
 console.log(numList = [3, 7, 9, 10, 0, 2, 5, 1, 8, 4]);
 console.log(bubbleSorter(numList));
 
-// 3.- 
-function invertArray()
+// EJERCICIO 3
+function newReversedArray(array) 
 {
-
-}
-
-function modifyArray()
-{
-
-}
-
-function newReversedArray(array) {
   let newNums = [];
   array.forEach(n => newNums.unshift(n));
   return newNums;
 }
 
-function reverseArray(array) {
-
-  for (let i = 0; i < array.length; i++) {
+function reverseArray(array) 
+{
+  for (let i = 0; i < array.length; i++) 
+  {
     array.splice(array.length - i, 0, array[0]);
     array.shift();
   }
-
 }
 
-console.log('Ejercicio 3: Una función que invierta un arreglo de números y regrese un nuevo arreglo con el resultado, otra función que modifique el mismo arreglo que se pasa como argumento.');
-
+console.log('EJERCICIO 3: Función que invierta un arreglo de números y regrese un nuevo arreglo con el resultado, otra función que modifique el mismo arreglo que se pasa como argumento.');
 let nums = [15, 34, 8, 24, 1];
 console.log("Array original: " + nums);
-
 let newNums = newReversedArray(nums);
 console.log("Nuevo array: " + newNums);
-
 reverseArray(nums);
 console.log("Mismo array invertido: " + nums);
 
 
-// 4.- Escribe una función que reciba una cadena de texto y regrese una nueva con la primer letra de cada palabra en mayúscula.
-function capitalizarPalabra(str) {
+// EJERCICIO 4
+function capitalizarPalabra(str) 
+{
   //llamar return para que regrese el resultado 
   return str
   //usar split para separar la frase en un arreglo de palabras
@@ -99,21 +93,22 @@ function capitalizarPalabra(str) {
     .join(' ');
 }
 
-console.log('Ejercicio 4: Función que recibe una cadena de texto y regresa una nueva con la primer letra de cada palabra en mayúscula.');
+console.log('EJERCICIO 4: Función que recibe una cadena de texto y regresa una nueva con la primer letra de cada palabra en mayúscula.');
 console.log("String original: 'hola mundo'")
 console.log(capitalizarPalabra('hola mundo'));
 
 
-// 5.- 
-//Crear funcion
-function MCD(x, y){
+// EJERCICIO 5
+function MCD(x, y)
+{
   //Crear variables para el mcd y el resto
   var mcd, rest;
   //Crear ciclo while para que se repita mientras x y y sean diferentes de 0
-  while (x != 0 && y != 0){
-  rest = x%y;
-  x = y;
-  y = rest;
+  while (x != 0 && y != 0)
+  {
+    rest = x%y;
+    x = y;
+    y = rest;
   } 
   //Si x es igual a 0, mcd es igual a y
   if(x==0) mcd =y;
@@ -122,7 +117,7 @@ function MCD(x, y){
   return mcd;
   }
 
-  console.log('Ejercicio 5: Función que calcula el máximo común divisor de dos números.');
+  console.log('EJERCICIO 5: Función que calcula el máximo común divisor de dos números.');
   console.log("Números: 10, 100")
   console.log(MCD(10,100))
 
@@ -134,45 +129,48 @@ function hackerSpeak()
     // console.log(cadena.replace('A', '4'))
 }
 
-// 7.-
-function factoriza(num){
+// EJERCICIO 7
+function factoriza(num)
+{
   let lista = [];
-  for(let i = 1; i <= num; i++){
-      if(num % i === 0){
+  for(let i = 1; i <= num; i++)
+  {
+      if(num % i === 0)
+      {
           lista.push(i);
       }
   }
   return lista;
 }
-console.log('Ejercicio 7: Función que recibe un número y regresa una lista con todos sus factores.');
+console.log('EJERCICIO 7: Función que recibe un número y regresa una lista con todos sus factores.');
 console.log("Número: 12")
 console.log('[' + factoriza(12) + ']');
 
 //console.log('Actividad 7: [' + factoriza(12)+']');
 
-
-// 8.- Escribe una función que quite los elementos duplicados de un arreglo y regrese una lista con los elementos que quedan. Por ejemplo: quitaDuplicados([1, 0, 1, 1, 0, 0]) -> [1, 0]
-function quitaDuplicados(array) {
+// EJERCICIO 8
+function quitaDuplicados(array)
+{
   return array.filter((item, i) => array.indexOf(item) === i);
 }
 
-console.log('Ejercicio 8: Función que quita los elementos duplicados de un arreglo y regresa una lista con los elementos que quedan.');
-
+console.log('EJERCICIO 8: Función que quita los elementos duplicados de un arreglo y regresa una lista con los elementos que quedan.');
 let numsRepetidos = [9, 3, 3, 3, 9, 1, 3, 1, 0, 1, 1, 0, 0];
 let numsNoRepetidos = quitaDuplicados(numsRepetidos);
 console.log("Array con números repetidos: " + numsRepetidos);
 console.log("Arary con números sin repetir: " + numsNoRepetidos);
 
-
-
-// 9.- Cadena más corta
-function longitudCadenaMasCorta(listaCadenas) {
+// EJERCICIO 9
+function longitudCadenaMasCorta(listaCadenas) 
+{
   // Inicializar la longitud mínima como la longitud de la primera cadena
   let longitudMinima = listaCadenas[0].length;
 
   // Recorrer la lista de cadenas y actualizar la longitud mínima si se encuentra una cadena más corta
-  for (let i = 1; i < listaCadenas.length; i++) {
-    if (listaCadenas[i].length < longitudMinima) {
+  for (let i = 1; i < listaCadenas.length; i++) 
+  {
+    if (listaCadenas[i].length < longitudMinima) 
+    {
       longitudMinima = listaCadenas[i].length;
     }
   }
@@ -181,18 +179,21 @@ function longitudCadenaMasCorta(listaCadenas) {
   return longitudMinima;
 }
 
+console.log('EJERCICIO 9: Función que recibe como parámetro una lista de cadenas de texto y regresa la longitud de la cadena más corta.');
 const listaCadenas = ['Memoria', 'Mundo', 'JavaScript', 'Mexico', 'genial'];
 console.log(longitudCadenaMasCorta(listaCadenas));
 
-
-// 10.-Escribe una función que revise si una cadena de texto es un palíndromo o no.
-function esPalindromo(cadena) {
+// EJERCICIO 10
+function esPalindromo(cadena) 
+{
   // Convertir la cadena a minúsculas y eliminar los espacios en blanco
   cadena = cadena.toLowerCase().replace(/\s/g, '');
 
   // Comparar la cadena original con su reverso
-  for (let i = 0; i < cadena.length / 2; i++) {
-    if (cadena[i] !== cadena[cadena.length - 1 - i]) {
+  for (let i = 0; i < cadena.length / 2; i++) 
+  {
+    if (cadena[i] !== cadena[cadena.length - 1 - i]) 
+    {
       return false;
     }
   }
@@ -204,12 +205,13 @@ const cadena1 = 'radar';
 const cadena2 = 'oso';
 const cadena3 = 'Hola mundo';
 
-console.log(esPalindromo(cadena1)); 
-console.log(esPalindromo(cadena2));
-console.log(esPalindromo(cadena3)); 
+console.log('EJERCICIO 10: Función que revisa si una cadena de texto es un palíndromo o no.')
+console.log('radar = ' + esPalindromo(cadena1)); 
+console.log('oso = ' + esPalindromo(cadena2));
+console.log('Hola mundo = ' + esPalindromo(cadena3)); 
 
 
-// 11.- Funcion para ordenar alfabeticamente las cadenas de texto en una lista
+// EJERCICIO 11
 
 let alphUnorderedList = ["Juan", "Pablo", "Pedro", "Angel", "Jose", "Octavio"];
 
@@ -228,22 +230,27 @@ function alphSortList(lst)
   }
   return lst;
 }
-console.log("\nlista desordenada: \n", alphUnorderedList)
+
+console.log('EJERCICIO 11: Funcion para ordenar alfabeticamente las cadenas de texto en una lista');
+console.log("\nLista desordenada: \n", alphUnorderedList)
 const alphOrderedList = alphSortList(alphUnorderedList);
 console.log("\nLista ordenada:\n",alphOrderedList);
 
-// 12.- Escribe una función que tome una lista de números y devuelva la mediana y la moda.
-//Ejercicio Mediana y Moda
-function medianaYModa(lista) {
+//EJERCICIO 12
+function medianaYModa(lista) 
+{
   // Ordena la lista de números en orden ascendente
   lista.sort(function(a, b){return a - b});
 
   // Calcula la mediana
   let mediana = 0;
   let longitud = lista.length;
-  if (longitud % 2 === 0) {
+  if (longitud % 2 === 0) 
+  {
     mediana = (lista[longitud/2 - 1] + lista[longitud/2]) / 2;
-  } else {
+  } 
+  else 
+  {
     mediana = lista[(longitud - 1) / 2];
   }
 
@@ -251,16 +258,21 @@ function medianaYModa(lista) {
   let contador = {};
   let maximo = 0;
   let moda = [];
-  for (let i = 0; i < longitud; i++) {
+  for (let i = 0; i < longitud; i++) 
+  {
     let numero = lista[i];
-    if (contador[numero] === undefined) {
+    if (contador[numero] === undefined) 
+    {
       contador[numero] = 0;
     }
     contador[numero]++;
-    if (contador[numero] > maximo) {
+    if (contador[numero] > maximo) 
+    {
       maximo = contador[numero];
       moda = [numero];
-    } else if (contador[numero] === maximo) {
+    } 
+    else if (contador[numero] === maximo) 
+    {
       moda.push(numero);
     }
   }
@@ -269,10 +281,12 @@ function medianaYModa(lista) {
   return { mediana: mediana, moda: moda };
 }
 
+console.log('EJERCICIO 12: Función que tome una lista de números y devuelva la mediana y la moda.');
 let lista = [1, 2, 3, 4, 5, 6, 6, 7, 7, 8, 9];
 let resultado = medianaYModa(lista);
-console.log(resultado.mediana); 
-console.log(resultado.moda);
+console.log('Lista = [1, 2, 3, 4, 5, 6, 6, 7, 7, 8, 9]');
+console.log('Mediana: ' + resultado.mediana); 
+console.log('Moda: ' + resultado.moda);
 
 // 13.- Funcion pra determinar cual es la cadena mas frecuente dentro de una lista de cadenas de texto
 
@@ -309,13 +323,16 @@ const mostFrequentData=FrequentString(lst)
 console.log("\nValores de salida: \n\n| Cadena mas frecuente | Numero de repeticiones | Acumulador de todos los elementos | \n", mostFrequentData);
 
 
-// 14.- Escribe una función que tome un número y devuelva verdadero si es una potencia de dos, falso de lo contrario.
+// EJERCICIO 14
 function PotenciaDeDos(num){
-  if(num<1){
+  if(num<1)
+  {
       return false;
   }
-  while(num > 1){
-      if(num % 2 !== 0){
+  while(num > 1)
+  {
+      if(num % 2 !== 0)
+      {
           return false;
       }
       num /= 2;
@@ -323,9 +340,10 @@ function PotenciaDeDos(num){
   return true;
 }
 
-console.log('Actividad 14: ' + PotenciaDeDos(4));
+console.log('EJERCICIO 14: Escribe una función que tome un número y devuelva verdadero si es una potencia de dos, falso de lo contrario.')
+console.log('Número = 4 --> ' + PotenciaDeDos(4));
 
-// 15.- 
+// EJERCICIO 15
 let list = [24, 36, 12, 5, 30, 99, 1];
 
 function descendingArray(array) 
@@ -346,10 +364,7 @@ function descendingArray(array)
     return array
   }
 
-// console.log('Ejercicio 15: Escribe una función que tome una lista de números y devuelva una nueva lista con todos los números en orden descendente.')
-// console.log("Lista desordenada:\n", list)
-// const orderedList=descendingArray(list);
-// console.log("Lista ordenada: \n", orderedList); 
-
+console.log('EJERCICIO 15: Escribe una función que tome una lista de números y devuelva una nueva lista con todos los números en orden descendente.')
+console.log("Lista desordenada:\n", list)
 const orderedList=descendingArray(list);
-console.log('Ejercicio 15: Escribe una función que tome una lista de números y devuelva una nueva lista con todos los números en orden descendente.\n Lista desordenada:\n' + list + '\n Lista ordenada: \n' + orderedList);
+console.log("Lista ordenada: \n", orderedList); 
